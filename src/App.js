814aaff28a3144244398db44ac8f035724e8f7f6
Gabriella Vishnevetskaya <vishnevetskayag@gmail.com>
2020-11-11 16:03:09 +0300

    components2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
-    </Navbar>
-    <Menu />
-  </div>
-  );
+    );
+  }
 }
 
 export default App;
