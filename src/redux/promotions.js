ad53444798f184c550c95446a99bad93ddd25072
Gabriella Vishnevetskaya <vishnevetskayag@gmail.com>
2020-12-06 12:10:59 +0100

    fetch handling errors

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,8 +1,9 @@
 import * as ActionTypes from './ActionTypes';
 
-export const Promotions = (state  = { isLoading: true,
-                                        errMess: null,
-                                        promotions:[]}, action) => {
+export const Promotions = (state  = { 
+    isLoading: true,
+    errMess: null,
+    promotions:[]}, action) => {
     switch (action.type) {
         case ActionTypes.ADD_PROMOS:
         return {...state, isLoading: false, errMess: null, promotions: action.payload};
